fbf9e0c48d1d816fec84b04580a9f650b02105c9
Atılhan Emre Dursunoğlu <atilhandursunoglu@outlook.com>
2016-08-20 22:09:36 +0300

    fixed most of release date

--- click.py
+++ click.py
@@ -6,19 +6,14 @@ import json
 import time
 
 moviesString = """[
-  ["The Others",2001,"Movie",8],
   ["Evangelion 1.01: You Are (Not) Alone",2007,"Movie",8],
   ["Memento",2000,"Movie",7],
   ["300",2006,"Movie",7],
   ["God Bless America",2011,"Movie",8],
   ["The Raid",2011,"Movie",8],
-  ["500 Days of Summer",2009,"Movie",9],
-  ["Wall-E",2008,"Movie",8],
   ["Kill Bill 2",2004,"Movie",8],
   ["Kill Bill 1",2003,"Movie",8],
   ["V For Vendetta",2005,"Movie",8],
-  ["Die Hard 4: Live Free or Die Hard",2007,"Movie",8],
-  ["Leon: The Professional",1994,"Movie",10],
   ["Person of Interest",2011,"TV-Series",10],
   ["Suits",2011,"TV-Series",9],
   ["Veronica Mars",2004,"TV-Series",10]
@@ -42,8 +37,9 @@ def SearchMovie(movie):
 	for imdbMovie in imdbSearch:
 		if int(imdbMovie['year']) == movie[1]:
 			break
-	searchString = imdbMovie['title'] 
-	yearSuffix = " (" + imdbMovie['year'] + ")"
+	imdbMovie = imdb.get_title_by_id(imdbMovie['imdb_id'])
+	searchString = imdbMovie.title 
+	yearSuffix = " (" + imdbMovie.release_date[:4] + ")"
 	click(S("#i_searchbox"))
 	write(searchString, into = S("#i_searchbox"))
 	try:
